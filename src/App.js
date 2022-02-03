4037b31081363a39ae5e2fb501ec1328e13af92e
Pratik733 <pratik.tayade733@gmail.com>
2022-02-03 21:46:00 +0530

    pricing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Curriculum from './components/curriculum/Curriculum';
+import KandA from './components/K-and-Aprice/K-and-A';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route exact path='/curriculum' element={<Curriculum />} />
+          <Route exact path='/KidsAndAdolescentsCourses' element={<KandA />} />
         </Routes>
         <Footer />
       </Router>
