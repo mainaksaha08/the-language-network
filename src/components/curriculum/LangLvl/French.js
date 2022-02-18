e81efed223c5e443e5ddcebc3a7551dba29023f0
MAINAKSAHA07 <mainaksaha@ternaengg.ac.in>
2022-02-18 12:35:11 +0530

    Update French.js

--- src/components/curriculum/LangLvl/French.js
+++ src/components/curriculum/LangLvl/French.js
@@ -9,7 +9,7 @@ export default function French() {
                     <h1 class="title-font text-xl font-medium mb-3" style={{ color: '#13bfab' }}>Introduction to French</h1>
                     <h2 class="tracking-widest text-lg title-font font-bold text-black mb-1">Curriculum Includes</h2>
                     <p class="leading-relaxed mb-3">Basic greetings and Introduction, Phonetics,Grammar and Vocabulary</p>
-                    <a href='' class="text-teal-500 hover:text-teal-400 inline-flex items-center">View detailed Curriculum</a>
+                    <a href='https://drive.google.com/file/d/1j_6O1oWukf3Q6nSl-pbZBdF1tvKte_2v/view?usp=sharing' class="text-teal-500 hover:text-teal-400 inline-flex items-center">View detailed Curriculum</a>
                 </div>
             </div>
             <div class="p-4 lg:w-1/4 fadeIN">
@@ -18,7 +18,7 @@ export default function French() {
                     <h1 class="title-font text-xl font-medium mb-3" style={{ color: '#13bfab' }}>Elementary Knowledge</h1>
                     <h2 class="tracking-widest text-lg title-font font-bold text-black mb-1">Curriculum Includes</h2>
                     <p class="leading-relaxed mb-3">Simple communication, Grammar and Vocabulary</p>
-                    <a href='' class="text-teal-500 hover:text-teal-400 inline-flex items-center">View detailed Curriculum</a>
+                    <a href='https://drive.google.com/file/d/1T42Mv-EZ96IdK5ZgQCkzQmqjL-5aKko5/view?usp=sharing' class="text-teal-500 hover:text-teal-400 inline-flex items-center">View detailed Curriculum</a>
                 </div>
             </div>
             <div class="p-4 lg:w-1/4 fadeIN">
@@ -27,7 +27,7 @@ export default function French() {
                     <h1 class="title-font text-xl font-medium mb-3" style={{ color: '#13bfab' }}>Intermediate Knowledge</h1>
                     <h2 class="tracking-widest text-lg title-font font-bold text-black mb-1">Curriculum Includes</h2>
                     <p class="leading-relaxed mb-3">Cultural & Social know-hows, Complex Grammar and Professional Vocabulary</p>
-                    <a href='' class="text-teal-500 hover:text-teal-400 inline-flex items-center">View detailed Curriculum</a>
+                    <a href='https://drive.google.com/file/d/1G-Xe3UXs-tG4FKnqKoFz9x0enn3FFiRQ/view?usp=sharing' class="text-teal-500 hover:text-teal-400 inline-flex items-center">View detailed Curriculum</a>
                 </div>
             </div>
             <div class="p-4 lg:w-1/4 fadeIN">
@@ -36,7 +36,7 @@ export default function French() {
                     <h1 class="title-font text-xl font-medium mb-3" style={{ color: '#13bfab' }}>Professional Knowledge</h1>
                     <h2 class="tracking-widest text-lg title-font font-bold text-black mb-1">Curriculum Includes</h2>
                     <p class="leading-relaxed mb-3">Degree of fluency, Express Opinions, Complex Grammar Structures.</p>
-                    <a href='' class="text-teal-500 hover:text-teal-400 inline-flex items-center">View detailed Curriculum</a>
+                    <a href='https://drive.google.com/file/d/1emNH98yEyWPEuLqEcEgdcJR1BCTcDAhz/view?usp=sharing' class="text-teal-500 hover:text-teal-400 inline-flex items-center">View detailed Curriculum</a>
                 </div>
             </div>
         </div>
